db13a9e0b0dd980ac47033ea49637bc5a2661b74
saadbadshah <sb135@hw.ac.uk>
2021-05-13 02:32:39 +0100

    Exceptions Handled

--- Tweets.py
+++ Tweets.py
@@ -15,7 +15,7 @@ nltk.download('punkt')
 pd.set_option('use_inf_as_na', True)
 class Get_Tweets():
     def __init__(self):
-        self.import_tweets()
+        #self.import_tweets()
         self.raw_tweets()
         self.clean_tweets()
         self.search_tweets()
@@ -35,48 +35,64 @@ class Get_Tweets():
         user = 'TheStreet'
         api = self.connect()
         tweets = tweepy.Cursor(api.user_timeline, screen_name=user).items(5)
-
         return tweets
 
 
     def makingdf(self, firstColum):
-        tweets = self.import_tweets()
-        StockTweets = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=[firstColum])
 
-        return StockTweets
+        try:
+            tweets = self.import_tweets()
+            StockTweets = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=[firstColum])
+        except tweepy.TweepError:
+            pass
+        else:
+            return StockTweets
 
     def raw_tweets(self):
-        raw_tweets = self.makingdf('Raw_Tweets')
-        raw_tweets['TxtBlob'] = raw_tweets['Raw_Tweets'].apply(self.txtblob_polarity)
-        raw_tweets['Nltk'] = raw_tweets['Raw_Tweets'].apply(self.nltk_polarity)
-
-        print(raw_tweets.to_string())
+        try:
+            raw_tweets = self.makingdf('Raw_Tweets')
+            raw_tweets['TxtBlob'] = raw_tweets['Raw_Tweets'].apply(self.txtblob_polarity)
+            raw_tweets['Nltk'] = raw_tweets['Raw_Tweets'].apply(self.nltk_polarity)
+        except TypeError:
+            print("The twitter user account entered not found")
+        else:
+            print(raw_tweets.to_string())
 
     def clean_data(self,clean_tweets):
-        clean_tweets['Clean_Tweets'] = clean_tweets['Clean_Tweets'].apply(self.cleaning_tweets).apply(lambda x: self.tokenize_tweets(x.lower()))
-        clean_tweets['Clean_Tweets'] = clean_tweets['Clean_Tweets'].apply(self.remove_stopwords).apply(self.remove_special_char)
+        try:
+            clean_tweets['Clean_Tweets'] = clean_tweets['Clean_Tweets'].apply(self.cleaning_tweets).apply(lambda x: self.tokenize_tweets(x.lower()))
+            clean_tweets['Clean_Tweets'] = clean_tweets['Clean_Tweets'].apply(self.remove_stopwords).apply(self.remove_special_char)
 
-        clean_tweets['TxtBlob'] = clean_tweets['Clean_Tweets'].apply(self.txtblob_polarity)
-        clean_tweets['Nltk'] = clean_tweets['Clean_Tweets'].apply(self.nltk_polarity)
-
-        return clean_tweets
+            clean_tweets['TxtBlob'] = clean_tweets['Clean_Tweets'].apply(self.txtblob_polarity)
+            clean_tweets['Nltk'] = clean_tweets['Clean_Tweets'].apply(self.nltk_polarity)
+        except TypeError:
+            pass
+        else:
+            return clean_tweets
 
     def clean_tweets(self):
-        clean_tweets = self.makingdf('Clean_Tweets')
-        clean_tweets = self.clean_data(clean_tweets)
-        print(clean_tweets.to_string())
+        try:
+            clean_tweets = self.makingdf('Clean_Tweets')
+            clean_tweets = self.clean_data(clean_tweets)
+            print(clean_tweets.to_string())
+        except AttributeError:
+            pass
 
     def search_tweets(self):
-        StockTweets = self.makingdf('Clean_Tweets')
-        Stock = 'HNST'
-        StockTweets['Clean_Tweets'] = StockTweets[StockTweets['Clean_Tweets'].str.contains(pat=Stock, case=False)]
-        StockTweets = StockTweets[StockTweets['Clean_Tweets'].notna()]
+        try:
+            StockTweets = self.makingdf('Clean_Tweets')
+            Stock = 'HNST'
+            StockTweets['Clean_Tweets'] = StockTweets[StockTweets['Clean_Tweets'].str.contains(pat=Stock, case=False)]
+            StockTweets = StockTweets[StockTweets['Clean_Tweets'].notna()]
 
-        StockTweets = self.clean_data(StockTweets)
-        print(StockTweets.to_string())
+            StockTweets = self.clean_data(StockTweets)
+            print(StockTweets.to_string())
 
-        stock_Ticker = input("Please enter the Stock Ticker Symbol for the data do be fetched(i.e NFLX): ")
-        StockData(stock_Ticker)
+            stock_Ticker = input("Please enter the Stock Ticker Symbol for the data do be fetched(i.e NFLX): ")
+            StockData(stock_Ticker)
+        except TypeError:
+            pass
+
 
     def nltk_polarity(self, tweets):
         polarity = SentimentIntensityAnalyzer().polarity_scores(tweets)
@@ -104,7 +120,7 @@ class Get_Tweets():
         tweet = ''.join(tweeet)
         return tweet
 
-#https://github.com/pjwebdev/Basic-Data-Science-Projects/blob/master/8-Twitter-Sentiment-Analysis/Tweeter%20Sentiment%20Analysis.ipynb the def contains regexes taken from this project
+
     def cleaning_tweets(self, txt):
         # Remove mentions
         txt = re.sub(r'@[A-Za-z0-9_]+', '', txt)
